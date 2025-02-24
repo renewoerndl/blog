0cf0fe07fe0ab0b627384dff3e4350225e06d13e
rene404 <rene@chickenvillage.com>
2025-02-24 14:15:01 +0100

    config class

---
+++ src/main/java/org/rene/blog/config/DatabaseConfig.java
@@ -0,0 +1,33 @@
+package org.rene.blog.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${database.url}")
+    private String url;
+
+    @Value("${database.username}")
+    private String username;
+
+    @Value("${database.password}")
+    private String password;
+
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName("com.postgresql.Driver");
+        return dataSource;
+    }
+}
