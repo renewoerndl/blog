7a77a34d56599736ae604d33c47e103c77790a6f
rene404 <rene@chickenvillage.com>
2025-02-27 09:38:59 +0100

    add/update comments comments

--- src/main/java/org/rene/blog/dto/TagDTO.java
+++ src/main/java/org/rene/blog/dto/TagDTO.java
@@ -2,6 +2,30 @@ package org.rene.blog.dto;
 
 import org.rene.blog.model.Tag;
 
+/**
+ * TagDTO is a Data Transfer Object for the Tag entity.
+ * It is used to transfer data between different layers of the application.
+ *
+ * Attributes
+ * ----------
+ * id : Long
+ *     The unique identifier of the tag.
+ * name : String
+ *     The name of the tag.
+ *
+ * Methods
+ * -------
+ * TagDTO(Tag tag)
+ *     Constructor that initializes the TagDTO with a Tag entity.
+ * getId()
+ *     Returns the unique identifier of the tag.
+ * setId(Long id)
+ *     Sets the unique identifier of the tag.
+ * getName()
+ *     Returns the name of the tag.
+ * setName(String name)
+ *     Sets the name of the tag.
+ */
 public class TagDTO {
     private Long id;
     private String name;
